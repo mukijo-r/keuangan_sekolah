b9982c0a2f83cc97f619e22bcb82ba7b7da5887e
mukijo-r <mukijo@student.unsia.ac.id>
2023-10-13 13:21:35 +0700

    menghapus footer

--- penetapan.php
+++ penetapan.php
@@ -149,18 +149,6 @@ require 'config.php';
                         </div>
                     </div>
                 </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
@@ -193,7 +181,7 @@ require 'config.php';
                                 <option value="0">Semua</option>
                                 <?php
                                 // Ambil data kelas dari tabel kelas
-                                $querySiswa = mysqli_query($conn, "SELECT id_siswa, nama FROM siswa");
+                                $querySiswa = mysqli_query($conn, "SELECT id_siswa, nama FROM siswa ORDER BY nama");
                                 while ($siswa = mysqli_fetch_assoc($querySiswa)) {
                                     echo '<option value="' . $siswa['id_siswa'] . '">' . $siswa['nama'] . '</option>';
                                 }
@@ -247,7 +235,7 @@ require 'config.php';
                                 <option value="0">Semua</option>
                                 <?php
                                 // Ambil data kelas dari tabel kelas
-                                $querySiswa = mysqli_query($conn, "SELECT id_siswa, nama FROM siswa");
+                                $querySiswa = mysqli_query($conn, "SELECT id_siswa, nama FROM siswa ORDER BY nama");
                                 while ($siswa = mysqli_fetch_assoc($querySiswa)) {
                                     echo '<option value="' . $siswa['id_siswa'] . '">' . $siswa['nama'] . '</option>';
                                 }
@@ -301,7 +289,7 @@ require 'config.php';
                                 <option value="0">Semua</option>
                                 <?php
                                 // Ambil data kelas dari tabel kelas
-                                $querySiswa = mysqli_query($conn, "SELECT id_siswa, nama FROM siswa");
+                                $querySiswa = mysqli_query($conn, "SELECT id_siswa, nama FROM siswa ORDER BY nama");
                                 while ($siswa = mysqli_fetch_assoc($querySiswa)) {
                                     echo '<option value="' . $siswa['id_siswa'] . '">' . $siswa['nama'] . '</option>';
                                 }
