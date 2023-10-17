5cffcf865f3cf810bf64993e955d9a978a7002c0
mukijo-r <mukijo@student.unsia.ac.id>
2023-10-17 18:47:39 +0700

    menambahkan menu register dan ganti password

--- navbar.php
+++ navbar.php
@@ -44,6 +44,8 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#modalRegister">Register</a></li>
+                        <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#modalGantiPassword">Ganti password</a></li>
                         <li><a class="dropdown-item" href="logout.php">Logout</a></li>
                     </ul>
                 </li>
