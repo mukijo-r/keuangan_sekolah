327efaa5a51c4fd11086d8cdde6d7d7565056d2c
mukijo-r <mukijo@student.undia.ac.id>
2023-10-24 16:33:30 +0700

    menambahkan variabel untuk bulan sekarang

--- config.php
+++ config.php
@@ -47,5 +47,33 @@ if ($bulanLalu == 'January') {
     $bulanLalu = 'Bulan Tidak valid';
 }
 
+$bulanIni = date('F');
+if ($bulanIni == 'January') {
+    $bulanIni = 'Januari';
+} elseif ($bulanIni == 'February') {
+    $bulanIni = 'Februari';
+} elseif ($bulanIni == 'March') {
+    $bulanLalu = 'Maret';
+} elseif ($bulanIni == 'April') {
+    $bulanIni = 'April';
+} elseif ($bulanIni == 'May') {
+    $bulanIni = 'Mei';
+} elseif ($bulanIni == 'June') {
+    $bulanIni = 'Juni';
+} elseif ($bulanIni == 'July') {
+    $bulanIni = 'Juli';
+} elseif ($bulanIni == 'August') {
+    $bulanIni = 'Agustus';
+} elseif ($bulanIni == 'September') {
+    $bulanIni = 'September';
+} elseif ($bulanIni == 'October') {
+    $bulanIni = 'Oktober';
+} elseif ($bulanIni == 'November') {
+    $bulanIni = 'November';
+} elseif ($bulanIni == 'December') {
+    $bulanIni = 'Desember';
+} else {
+    $bulanIni = 'Bulan Tidak valid';
+}
 
 ?>
