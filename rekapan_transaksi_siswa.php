3fa5820b8a9829c49ebfc4c0d22b0c8bce5a9bd7
mukijo-r <mukijo@student.unsia.ac.id>
2023-10-25 20:15:54 +0700

    memperbaiki perhitungan total penetapan

--- rekapan_transaksi_siswa.php
+++ rekapan_transaksi_siswa.php
@@ -979,7 +979,6 @@ $conn = mysqli_connect("localhost:3306","root","","sdk");
                     for ($kelas = 1; $kelas <= 6; $kelas++) {
 
                     $queryTotal = "SELECT
-                    SUM(CASE WHEN s.id_kelas = $kelas AND subkat.nama_sub_kategori = 'SPP' THEN tms.penetapan ELSE 0 END) AS penetapan_spp_kelas,
                     SUM(CASE WHEN s.id_kelas = $kelas AND subkat.nama_sub_kategori = 'SPP' THEN tms.bulan_ini ELSE 0 END) AS bulan_ini_spp_kelas,
                     SUM(CASE WHEN s.id_kelas = $kelas AND subkat.nama_sub_kategori = 'SPP' THEN tms.tunggakan ELSE 0 END) AS tunggakan_spp_kelas,
                     SUM(CASE WHEN s.id_kelas = $kelas AND subkat.nama_sub_kategori = 'SPP' THEN tms.jumlah ELSE 0 END) AS total_spp_kelas
@@ -997,11 +996,22 @@ $conn = mysqli_connect("localhost:3306","root","","sdk");
                     $resultTotal = mysqli_query($conn, $queryTotal);
 
                     $rowKelas=mysqli_fetch_array($resultTotal);
-                    $penetapanSppKelas =  $rowKelas['penetapan_spp_kelas'];
+                    
                     $bulanIniSppKelas =  $rowKelas['bulan_ini_spp_kelas'];
                     $tunggakanSppKelas =  $rowKelas['tunggakan_spp_kelas'];
                     $totalSppKelas = $rowKelas['total_spp_kelas'];
-                    
+
+                    $queryPenetapanKelas = "SELECT SUM(p.nominal) AS total_nominal
+                    FROM penetapan p
+                    JOIN siswa s ON p.id_siswa = s.id_siswa
+                    WHERE
+                    p.id_sub_kategori = 5 AND
+                    s.id_kelas = $kelas;";
+
+                    $resultPenetapanSppKelas = mysqli_query($conn, $queryPenetapanKelas);                    
+                    $rowPenetapanSppKelas = mysqli_fetch_array($resultPenetapanSppKelas);
+                    $penetapanSppKelas = $rowPenetapanSppKelas['total_nominal'];
+
                     echo '<tr>';
                     echo '<td>Kelas ' . $kelas . '</td>';
                     echo '<td>Rp. ' . number_format($penetapanSppKelas, 0, ',', '.') . '</td>';
